b0a079b878f1ddcd3adf69f42dd0ce1c26bdeca1
Zojae031 <edgar3@naver.com>
2020-01-03 17:34:20 +0900

    Add Repository Comment

--- Server/src/data/Repository.java
+++ Server/src/data/Repository.java
@@ -1,5 +1,11 @@
 package data;
 
+/**
+ * 데이터의 분기를 위한 Repository interface
+ * 기존 non_blocking 으로 이루어져있었고, 다중 클라이언트를 지원할 예정이었기 때문에 여러 함수들이 사용되지 않고 남아있다.
+ *
+ * @author 조재영
+ */
 public interface Repository {
     void connectClient();
 
